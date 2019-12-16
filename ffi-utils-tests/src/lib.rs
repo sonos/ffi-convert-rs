37592f1d318578bc3498203c671b866eef3ddea6
Deluvi <noe.baylac@protonmail.com>
2019-12-16 17:48:56 +0100

    Add automatic round-trip tests facilities for every CReprOf/AsRust compatible struct

--- ffi-utils-tests/src/lib.rs
+++ ffi-utils-tests/src/lib.rs
@@ -1,7 +1,40 @@
+use failure::{bail, Fallible};
+use ffi_utils::{AsRust, CReprOf};
+
+#[macro_export]
+macro_rules! generate_round_trip_rust_c_rust {
+    ($func_name:ident, $rust_struct:path, $c_struct:path, $builder:block) => {
+        #[test]
+        fn $func_name() {
+            use $crate::round_trip_test_rust_c_rust;
+            let item = $builder;
+            round_trip_test_rust_c_rust::<$c_struct, $rust_struct>(item)
+                .expect("Round trip test failed!");
+        }
+    };
+}
+
+pub fn round_trip_test_rust_c_rust<T, U>(value: U) -> Fallible<()>
+where
+    T: AsRust<U> + CReprOf<U>,
+    U: Clone + PartialEq,
+{
+    let value2 = value.clone();
+    let intermediate: T = T::c_repr_of(value2)?;
+    let value_roundtrip: U = intermediate.as_rust()?;
+
+    if value != value_roundtrip {
+        bail!("The value is not the same before and after the roundtrip");
+    }
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use ffi_utils::{AsRust, CArray, CReprOf};
 
+    #[derive(Clone, Debug, PartialEq)]
     pub struct Pancake {
         pub start: f32,
         pub end: f32,
@@ -21,6 +54,7 @@ mod tests {
         toppings: CArray<CTopping>,
     }
 
+    #[derive(Clone, Debug, PartialEq)]
     pub struct Sauce {
         pub volume: f32,
     }
@@ -32,6 +66,7 @@ mod tests {
         volume: f32,
     }
 
+    #[derive(Clone, Debug, PartialEq)]
     pub struct Topping {
         pub amount: i32,
     }
@@ -43,6 +78,7 @@ mod tests {
         amount: i32,
     }
 
+    #[derive(Clone, Debug, PartialEq)]
     pub struct Dummy {
         pub count: i32,
     }
@@ -54,16 +90,21 @@ mod tests {
         count: i32,
     }
 
-    #[test]
-    fn should_work() {
-        let pancakes = Pancake {
+    generate_round_trip_rust_c_rust!(round_trip_sauce, Sauce, CSauce, { Sauce { volume: 4.2 } });
+
+    generate_round_trip_rust_c_rust!(round_trip_topping, Topping, CTopping, {
+        Topping { amount: 2 }
+    });
+
+    generate_round_trip_rust_c_rust!(round_trip_dummy, Dummy, CDummy, { Dummy { count: 2 } });
+
+    generate_round_trip_rust_c_rust!(round_trip_pancake, Pancake, CPancake, {
+        Pancake {
             start: 0.0,
             end: 2.0,
             dummy: Dummy { count: 2 },
-            sauce: None,
+            sauce: Some(Sauce { volume: 4.2 }),
             toppings: vec![Topping { amount: 2 }, Topping { amount: 3 }],
-        };
-
-        let _c_pancakes = CPancake::c_repr_of(pancakes).unwrap();
-    }
+        }
+    });
 }
