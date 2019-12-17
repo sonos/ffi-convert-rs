d19704180df7bdd7ad3c5f82c2ceb3b5d781a423
CoorFun <coorfang@gmail.com>
2019-12-17 18:00:38 +0100

    Add test for optional fields and redirectionable entity

--- ffi-utils-tests/src/lib.rs
+++ ffi-utils-tests/src/lib.rs
@@ -17,8 +17,9 @@ mod tests {
         start: f32,
         end: f32,
         dummy: CDummy,
+        #[nullable]
         sauce: *const CSauce,
-        toppings: CArray<CTopping>,
+        toppings: *const CArray<CTopping>
     }
 
     pub struct Sauce {
