eebf010219c37ee7365fb78ddcb84433a9be27e8
anthonyray <dantho361@hotmail.com>
2019-12-17 13:33:51 +0100

    Fix impl of CReprOf trait for CArray type for empty array cases

--- ffi-utils/src/types.rs
+++ ffi-utils/src/types.rs
@@ -1,4 +1,5 @@
 use std::ffi::CString;
+use std::ptr::null;
 
 use failure::{Error, ResultExt};
 
@@ -84,28 +85,37 @@ pub struct CArray<T> {
 
 impl<U: AsRust<V>, V> AsRust<Vec<V>> for CArray<U> {
     fn as_rust(&self) -> Result<Vec<V>, Error> {
-        let values = unsafe { std::slice::from_raw_parts_mut(self.data_ptr as *mut U, self.size) };
-        let mut vec = Vec::with_capacity(values.len());
-        for value in values {
-            vec.push(value.as_rust()?);
+        let mut vec = Vec::with_capacity(self.size);
+        if self.size > 0 {
+            let values = unsafe { std::slice::from_raw_parts_mut(self.data_ptr as *mut U, self.size) };
+            for value in values {
+                vec.push(value.as_rust()?);
+            }
         }
-
         Ok(vec)
     }
 }
 
 impl<U: CReprOf<V>, V> CReprOf<Vec<V>> for CArray<U> {
     fn c_repr_of(input: Vec<V>) -> Result<Self, Error> {
-        let mut output_array: Vec<U> = Vec::new();
-        for input_item in input {
-            output_array.push(U::c_repr_of(input_item)?);
-        }
-        let size = output_array.len();
-        let output_array = output_array.into_boxed_slice();
-        Ok(Self {
-            data_ptr: Box::into_raw(output_array) as *const U,
-            size,
-        })
+        let input_size = input.len();
+        Ok(
+            Self {
+                data_ptr: if input_size > 0 {
+                    Box::into_raw(
+                        input
+                            .into_iter()
+                            .map(|item| U::c_repr_of(item))
+                            .collect::<Result<Vec<_>, Error>>()
+                            .expect("Could not convert to C representation")
+                            .into_boxed_slice()
+                    ) as *const U
+                } else {
+                    null() as *const U
+                },
+                size: input_size
+            }
+        )
     }
 }
 
