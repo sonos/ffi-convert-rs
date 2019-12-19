a41be6dcf9851259165a32e0148d1e98bc6c5829
CoorFun <coorfang@gmail.com>
2019-12-19 15:22:38 +0100

    Add another layer checking for pointer type, invoke marco! for libc::c_char

--- ffi-utils-derive/src/lib.rs
+++ ffi-utils-derive/src/lib.rs
@@ -35,29 +35,54 @@ fn impl_creprof_macro(input: &syn::DeriveInput) -> TokenStream {
         .map(|field|
             (field.ident.as_ref().expect("field should have an ident"),
              match &field.ty {
-                 Type::Ptr(ptr_t) => { match &*ptr_t.elem {
-                     Type::Path(path_t) => quote!(ffi_utils::RawPointerTo::< #path_t >),
-                     _ => panic!("")
-                 }}
-                 Type::Path(path_t) => { generic_path_to_concrete_type_path(&path_t.path) }
+                 Type::Ptr(ptr_t) => {
+                     match &*ptr_t.elem {
+                         Type::Path(path_t) => {
+                             if let Some(_it) = path_t.path.segments.iter().find(|it| {
+                                 it.ident.to_string().contains("c_char")
+                             }) {
+                                 // it's a pointer to str, return tuple of (composed_type, is_str_flag)
+                                 (quote!(ffi_utils::RawPointerTo::< #path_t >), true)
+                             } else {
+                                 (quote!(ffi_utils::RawPointerTo::< #path_t >), false)
+                             }
+                         }
+                         _ => panic!("")
+                     }
+                 }
+                 Type::Path(path_t) => { (generic_path_to_concrete_type_path(&path_t.path), false) }
                  _ => { panic!("") }
              },
              &field.attrs))
-        .map(|(field_name, field_type, field_attrs)| {
+        .map(|(field_name, (field_type, is_str), field_attrs)| {
             let nullable = field_attrs.iter().find(|attr| {
                 attr.path.get_ident().map(|it| it.to_string()) == Some("nullable".into())
             });
 
             if let Some(_it) = nullable {
-                quote!(
-                    #field_name: if let Some(it) = input.#field_name {
-                        #field_type::c_repr_of(it)?
-                    } else {
-                        std::ptr::null() as _
-                    }
-                )
+                if is_str {
+                    quote!(
+                        #field_name: if let Some(it) = input.#field_name {
+                            convert_to_c_string_result!(it)?
+                        } else {
+                            std::ptr::null() as _
+                        }
+                    )
+                } else {
+                    quote!(
+                        #field_name: if let Some(it) = input.#field_name {
+                            #field_type::c_repr_of(it)?
+                        } else {
+                            std::ptr::null() as _
+                        }
+                    )
+                }
             } else {
-                quote!(#field_name: #field_type ::c_repr_of(input.#field_name)?)
+                if is_str {
+                    quote!(#field_name: convert_to_c_string_result!(input.#field_name)?)
+                } else {
+                    quote!(#field_name: #field_type ::c_repr_of(input.#field_name)?)
+                }
             }
         })
         .collect::<Vec<_>>();
@@ -70,8 +95,7 @@ fn impl_creprof_macro(input: &syn::DeriveInput) -> TokenStream {
                 })
             }
         }
-    )
-    .into()
+    ).into()
 }
 
 fn generic_path_to_concrete_type_path(path: &syn::Path) -> proc_macro2::TokenStream {
@@ -80,7 +104,7 @@ fn generic_path_to_concrete_type_path(path: &syn::Path) -> proc_macro2::TokenStr
     let segments = &path.segments;
     let ident = &last_segment.value().ident;
     let turbofished_type = if let syn::PathArguments::AngleBracketed(bracketed_args) =
-        &last_segment.value().arguments
+    &last_segment.value().arguments
     {
         quote!(#ident::#bracketed_args)
     } else {
@@ -120,24 +144,58 @@ fn impl_asrust_macro(input: &syn::DeriveInput) -> TokenStream {
         .fields
         .iter()
         .map(|field|
-            (field.ident.as_ref().expect("field should have an ident"),
-             &field.attrs)
+            (
+                field.ident.as_ref().expect("field should have an ident"),
+                match &field.ty {
+                    Type::Ptr(ptr_t) => {
+                        match &*ptr_t.elem {
+                            Type::Path(path_t) => {
+                                if let Some(_it) = path_t.path.segments.iter().find(|it| {
+                                    it.ident.to_string().contains("c_char")
+                                }) {
+                                    true
+                                } else {
+                                    false
+                                }
+                            }
+                            _ => panic!("")
+                        }
+                    }
+                    Type::Path(_path_t) => false,
+                    _ => { panic!("") }
+                },
+                &field.attrs
+            )
         )
-        .map(|(field_name, field_attrs)| {
+        .map(|(field_name, is_str, field_attrs)| {
             let nullable = field_attrs.iter().find(|attr| {
                 attr.path.get_ident().map(|it| it.to_string()) == Some("nullable".into())
             });
 
             if let Some(_it) = nullable {
-                quote!(
-                    #field_name: if self.#field_name != std::ptr::null() {
-                        Some(self.#field_name.as_rust()?)
-                    } else {
-                        None
-                    }
-                )
+                if is_str {
+                    quote!(
+                        #field_name: if self.#field_name != std::ptr::null() {
+                            Some(create_rust_string_from!(self.#field_name)?)
+                        } else {
+                            None
+                        }
+                    )
+                } else {
+                    quote!(
+                        #field_name: if self.#field_name != std::ptr::null() {
+                            Some(self.#field_name.as_rust()?)
+                        } else {
+                            None
+                        }
+                    )
+                }
             } else {
-                quote!(#field_name : self.#field_name.as_rust()?)
+                if is_str {
+                    quote!(#field_name : create_rust_string_from!(self.#field_name))
+                } else {
+                    quote!(#field_name : self.#field_name.as_rust()?)
+                }
             }
         })
         .collect::<Vec<_>>();
@@ -150,6 +208,5 @@ fn impl_asrust_macro(input: &syn::DeriveInput) -> TokenStream {
                 })
             }
         }
-    )
-    .into()
+    ).into()
 }
